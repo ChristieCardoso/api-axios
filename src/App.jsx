7afd2ce84685bbdb385c8669fb31307d0e507c8a
Christie Cardoso <christiecardoso@hotmail.com>
2023-07-01 17:59:42 -0300

    adicionando axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const url = "https://api.randomuser.me/?results=3";
+  function getUser() {
+    axios
+      .get(url)
+      .then((response) => {
+        const data = response.data.results;
+            console.log('ei', data);
+      })
+      .catch((error) => console.log(error));
+  }
+  getUser();
+
   return (
     <div className="App">
-      <h1>
-        Edit save to test HMR
-      </h1>
+      <div>
+        <p id="renderResults">Edit save to test HMR</p>
+      </div>
     </div>
   );
 }

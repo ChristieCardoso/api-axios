66c91e90550b77b03da424fdf675da31d4e4e11a
Christie Cardoso <christiecardoso@hotmail.com>
2023-07-02 17:32:51 -0300

    criando a chamada com axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  const url = "https://api.randomuser.me/?results=3";
-  function getUser() {
-    axios
-      .get(url)
-      .then((response) => {
+  const url = "https://api.randomuser.me/?results=2";
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(url);
         const data = response.data.results;
-            console.log('ei', data);
-      })
-      .catch((error) => console.log(error));
-  }
-  getUser();
+        setUsers(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
       <div>
-        <p id="renderResults">Edit save to test HMR</p>
+        {users.map((user) => (
+          <div key={user.login.username} >
+            <img src={user.picture.large} alt={user.name.first} />
+            <h2>{user.name.first}</h2>
+            <p>Idade: {user.dob.age}</p>
+            <p>Sexo: {user.gender}</p>
+            <p>Cidade: {user.location.city}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
